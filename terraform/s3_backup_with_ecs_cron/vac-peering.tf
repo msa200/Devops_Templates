e8a9b6d3021553f8132c4dbaa364ee6d6764d659
Bassant Sayed <46677101+bassantSayed@users.noreply.github.com>
2021-04-07 22:48:08 +0200

    Create vac-peering.tf

---
+++ terraform/s3_backup_with_ecs_cron/vac-peering.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc_peering_connection" "peering" {
+  peer_owner_id = var.shared_services_acceptor_account_id
+  vpc_id        = aws_vpc.vpc.id
+  peer_vpc_id   = data.aws_vpc.shared_services_acceptor_vpc_tags.id
+  peer_region   = var.shared_services_acceptor_region
+  auto_accept   = false
+
+  tags = {
+    var.shared_services_1_acceptor_peering_tags
+    }
+
+
+
+}
+
+resource "aws_vpc_peering_connection_accepter" "acceptor" {
+  provider                  = aws.peer
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  auto_accept               = true
+
+  tags = {
+    Side = "Accepter"
+  }
+
+  depends_on = [
+    aws_vpc_peering_connection.peering
+  ]
+
+}
